decfcf0515882acab9875865561975ffb28949a4
unknown <s192237>
2020-12-08 15:24:14 +0900

    jsファイルに処理をまとめた

---
+++ firebaseImport.js
@@ -0,0 +1,4 @@
+document.write('<script src="https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js"></script>');
+document.write('<script src="https://www.gstatic.com/firebasejs/6.3.4/firebase-firestore.js"></script>');
+
+document.write('<script src="https://www.gstatic.com/firebasejs/5.9.1/firebase-messaging.js"></script>');
