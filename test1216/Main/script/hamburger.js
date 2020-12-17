9e6675c12c5271a28afa5f64a0808a50c209f861
unknown <s192302>
2020-12-17 10:17:29 +0900

    12月16日

---
+++ test1216/Main/script/hamburger.js
@@ -0,0 +1,22 @@
+function hamburger() {
+      document.getElementById('line1').classList.toggle('linea');
+      document.getElementById('line2').classList.toggle('lineb');
+      document.getElementById('line3').classList.toggle('linec');
+      document.getElementById('target').classList.toggle('slidex');
+      document.getElementById('nav_f').classList.toggle('fadein');
+    }
+
+
+    console.log(document.getElementById('td1').style.color);
+
+    document.getElementById('target').addEventListener('click', function() {
+      hamburger();
+    });
+
+    let list = document.getElementsByClassName('list');
+
+    for (let i = 0; i < list.length; i++) {
+      list[i].addEventListener('click', function() {
+        hamburger();
+      });
+    }
